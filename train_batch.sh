b43a340b8bc3d26b1b7db61ed40b16ecc4f94293
Henk Berendsen <hberendsen@cn84.science.ru.nl>
2025-06-08 10:37:10 +0200

    Decrease required resources

--- train_batch.sh
+++ train_batch.sh
@@ -3,9 +3,9 @@
 #SBATCH -p csedu-prio,csedu
 #SBATCH --qos=csedu-normal
 #SBATCH -c 2
-#SBATCH --mem 4G
+#SBATCH --mem 5G
 #SBATCH --gres=gpu:rtx_2080_ti:1 
-#SBATCH --time=6:00:00
+#SBATCH --time=2:30:00
 
 . ./input_validation.sh
 input_validation $@
