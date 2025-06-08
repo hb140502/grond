7e5602bb1a37b7fc333026bf35b392060a1160cf
Henk <hb130502@outlook.com>
2025-06-08 17:40:43 +0200

    Do not normalize UPGD trigger, but instead normalize adversarial input before feeding it to the model

--- generate_upgd.py
+++ generate_upgd.py
@@ -12,7 +12,7 @@ import os
 from tqdm import tqdm
 from pprint import pprint
 
-from utils_grond import set_seed, CIFAR10Poisoned, AverageMeter, accuracy_top1, transform_test, normalization, make_and_restore_model
+from utils_grond import set_seed, CIFAR10Poisoned, AverageMeter, accuracy_top1, normalization, make_and_restore_model
 from attacks.step import LinfStep, L2Step
 from utils_grond import show_image_row
 from poison_loader import folder_load
@@ -25,25 +25,7 @@ STEPS = {
     'L2': L2Step,
 }
 
-# From https://discuss.pytorch.org/t/simple-way-to-inverse-transform-normalization/4821/3
-class UnNormalize(object):
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, tensor):
-        """
-        Args:
-            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
-        Returns:
-            Tensor: Normalized image.
-        """
-        for t, m, s in zip(tensor, self.mean, self.std):
-            t.mul_(s).add_(m)
-            # The normalize code -> t.sub_(m).div_(s)
-        return tensor
-
-def universal_target_attack(model, loader, target_class, args):
+def universal_target_attack(model, loader, target_class, args, normalizer):
     delta = torch.zeros(1, *args.data_shape).to(args.device, non_blocking=True)
     orig_delta = delta.clone().detach()
     step = STEPS[args.constraint](orig_delta, args.eps, args.step_size)
@@ -69,7 +51,7 @@ def universal_target_attack(model, loader, target_class, args):
         delta = delta.clone().detach().requires_grad_(True)
         inp_adv = inp + delta
         inp_adv = torch.clamp(inp_adv, 0, 1)
-        logits = model(inp_adv)
+        logits = model(normalizer(inp_adv))
         loss = nn.CrossEntropyLoss()(logits, target)
         grad = torch.autograd.grad(loss, [delta])[0]
 
@@ -87,12 +69,9 @@ def universal_target_attack(model, loader, target_class, args):
 
 
 
-def upgd_generate(args, loader, model):
-    poison = universal_target_attack(model, loader, args.target_cls, args)
+def upgd_generate(args, loader, model, normalizer):
+    poison = universal_target_attack(model, loader, args.target_cls, args, normalizer)
     return poison.squeeze()
-
-
-
 
 def main(args):
     transforms_list = []
@@ -103,30 +82,33 @@ def main(args):
         args.data_shape = (args.channel, args.img_size, args.img_size)
         transforms_list.append(transforms.RandomResizedCrop(args.img_size))
         transforms_list.append(transforms.ToTensor()) 
-        transform = transforms.Compose(transforms_list)
-        data_set = torchvision.datasets.ImageFolder(root=args.data_root+'/imagenet200/train', transform=transform)
-        test_set = torchvision.datasets.ImageFolder(root=args.data_root+'/imagenet200/val', transform=transform)
+        transform_test = transforms.Compose(transforms_list)
+        data_set = torchvision.datasets.ImageFolder(root=args.data_root+'/imagenet200/train', transform=transform_test)
+        test_set = torchvision.datasets.ImageFolder(root=args.data_root+'/imagenet200/val', transform=transform_test)
     elif args.dataset=='cifar10':
         args.num_classes=10
         args.img_size  = 32
         args.channel   = 3
         args.data_shape = (args.channel, args.img_size, args.img_size)
-        data_set = datasets.CIFAR10(args.data_root, train=True, download=True, transform=transform_test[args.dataset])
-        test_set = datasets.CIFAR10(args.data_root, train=False, download=True, transform=transform_test[args.dataset])
+        transform_test = transforms.Compose([transforms.ToTensor()])
+        data_set = datasets.CIFAR10(args.data_root, train=True, download=True, transform=transform_test)
+        test_set = datasets.CIFAR10(args.data_root, train=False, download=True, transform=transform_test)
     elif args.dataset=='cifar100':
         args.num_classes=100
         args.img_size  = 32
         args.channel   = 3
         args.data_shape = (args.channel, args.img_size, args.img_size)
-        data_set = datasets.CIFAR100(args.data_root, train=True, download=True, transform=transform_test[args.dataset])
-        test_set = datasets.CIFAR100(args.data_root, train=False, download=True, transform=transform_test[args.dataset])
+        transform_test = transforms.Compose([transforms.ToTensor()])
+        data_set = datasets.CIFAR100(args.data_root, train=True, download=True, transform=transform_test)
+        test_set = datasets.CIFAR100(args.data_root, train=False, download=True, transform=transform_test)
     elif args.dataset=='tiny':
         args.num_classes=200
         args.img_size  = 64
         args.channel   = 3
         args.data_shape = (args.channel, args.img_size, args.img_size)
-        data_set = TinyImageNet(args.data_root, split="train", download=True, transform=transform_test[args.dataset])
-        test_set = TinyImageNet(args.data_root, split="val", download=True, transform=transform_test[args.dataset])
+        transform_test = transforms.Compose([transforms.ToTensor()])
+        data_set = TinyImageNet(args.data_root, split="train", download=True, transform=transform_test)
+        test_set = TinyImageNet(args.data_root, split="val", download=True, transform=transform_test)
     elif args.dataset=='gtsrb':
         args.num_classes=43
         args.img_size  = 32
@@ -135,9 +117,9 @@ def main(args):
         transforms_list.append(transforms.Resize(args.img_size))
         transforms_list.append(transforms.CenterCrop(args.img_size))
         transforms_list.append(transforms.ToTensor())
-        transform = transforms.Compose(transforms_list)
-        data_set = torchvision.datasets.ImageFolder(root=args.data_root+'/GTSRB/Train', transform=transform)
-        test_set = torchvision.datasets.ImageFolder(root=args.data_root+'/GTSRB/val4imagefolder', transform=transform)
+        transform_test = transforms.Compose(transforms_list)
+        data_set = torchvision.datasets.ImageFolder(root=args.data_root+'/GTSRB/Train', transform=transform_test)
+        test_set = torchvision.datasets.ImageFolder(root=args.data_root+'/GTSRB/val4imagefolder', transform=transform_test)
     else:
         print('Check dataset.')
         exit(0)
@@ -152,12 +134,8 @@ def main(args):
     # eval_model(args, model, test_loader, test_loader)
 
     set_seed(args.seed)
-    upgd = upgd_generate(args, data_loader, model)
-
-    # For these datasets we created the perturbation with normalized data, so we unnormalize the perturbation before saving
-    if args.dataset in ["cifar10", "cifar100", "tiny"]:
-        unnorm = UnNormalize(*normalization[args.dataset])
-        upgd = unnorm(upgd)
+    normalizer = transforms.Normalize(*normalization[args.dataset])
+    upgd = upgd_generate(args, data_loader, model, normalizer)
 
     file = 'upgd_'+str(args.target_cls)+'.pth'
     torch.save(upgd, os.path.join(args.upgd_path, file))
