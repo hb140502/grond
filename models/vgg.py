1d938efa56510315f24dbcd1da8a39eaf1685328
Henk <hb130502@outlook.com>
2025-07-17 18:13:29 +0200

    Modify custom VGG implementation to make compatible with stealthiness evaluation

--- models/vgg.py
+++ models/vgg.py
@@ -1,4 +1,14 @@
-'''VGG11/13/16/19 in Pytorch.'''
+'''VGG11/13/16/19 in Pytorch.
+
+Reference:
+[1] Karen Simonyan and Andrew Zisserman
+    Very Deep Convolutional Networks for Large-Scale Image Recognition. arXiv:1409.1556
+
+Note: 
+- To keep this VGG implementation consistent with the ResNet implementation, the "with_latent" argument to the forward method is renamed to "return_features", and the forward_all_features method is added.
+- nn.AvgPool2d is replaced with nn.AdaptiveAvgPool2d to make this VGG implementation also work with larger image sizes.
+- The torchsummary import is replaced by torchinfo, as the module has been renamed.
+'''
 import torch
 import torch.nn as nn
 
@@ -15,15 +25,33 @@ class VGG(nn.Module):
         self.features = self._make_layers(cfg[vgg_name])
         self.classifier = nn.Linear(512, num_classes)
 
-    def forward(self, x, with_latent=False, fake_relu=False, no_relu=False):
+    def forward(self, x, return_features=False, fake_relu=False, no_relu=False):
         assert (not fake_relu) and (not no_relu),  \
             "fake_relu and no_relu not yet supported for this architecture"
         out = self.features(x)
         latent = out.view(out.size(0), -1)
         out = self.classifier(latent)
-        if with_latent:
+        if return_features:
             return out, latent
         return out
+    
+    # Return features after every MaxPool2d layer
+    def forward_all_features(self, x):
+        n_layers = len(self.features)
+        maxpool_indices = [i for i in range(n_layers) if isinstance(self.features[i], nn.MaxPool2d)]
+        features = []
+        start = 0
+
+        for end in maxpool_indices:
+            x = self.features[start:end+1](x)
+            features.append(x)
+            start = end+1
+
+        x = self.features[start:](x)
+        latent = x.view(x.size(0), -1)
+        out = self.classifier(latent)
+
+        return out, features
 
     def _make_layers(self, cfg):
         layers = []
@@ -36,7 +64,7 @@ class VGG(nn.Module):
                            nn.BatchNorm2d(x),
                            nn.ReLU(inplace=True)]
                 in_channels = x
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        layers += [nn.AdaptiveAvgPool2d(output_size=(1,1))]
         return nn.Sequential(*layers)
 
 def VGG11(**kwargs):
@@ -53,6 +81,6 @@ def VGG19(**kwargs):
 
 
 if __name__ == "__main__":
-    from torchsummary import summary
+    from torchinfo import summary
     model = VGG16(num_classes=10)
-    summary(model, (3, 32, 32), device='cpu')
+    summary(model, (1, 3, 32, 32), device='cpu')
